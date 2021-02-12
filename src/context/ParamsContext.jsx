97f0050f0819a73fadd9841390d2e4bb8297aa37
eksite <denisandipod@gmail.com>
2021-02-12 15:27:53 +0300

    added params context

---
+++ src/context/ParamsContext.jsx
@@ -0,0 +1,51 @@
+import React, { useReducer, useContext } from "react";
+
+const ParamsStateContext = React.createContext();
+const ParamsReducerContext = React.createContext();
+
+const initialState = {
+  textLanguage: "ru",
+  textForType: "",
+};
+
+const paramsReducer = (action, state) => {};
+
+const ParamsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(paramsReducer, initialState);
+
+  const changeLanguage = (lang) => {
+    dispatch({ type: "CHANGE_LANGUAGE", lang });
+  };
+
+  const updateText = (text) => {
+    dispatch({ type: "UPDATE_TEXT", text });
+  };
+
+  return (
+    <ParamsStateContext.Provider value={state}>
+      <ParamsReducerContext.Provider
+        value={{ dispatch, changeLanguage, updateText }}
+      >
+        {children}
+      </ParamsReducerContext.Provider>
+    </ParamsStateContext.Provider>
+  );
+};
+
+const useParamsDispatch = () => {
+  const context = useContext(ParamsDispatchContext);
+  if (context === undefined) {
+    throw new Error("useParamsDispatch must be used within a ParamsProvider");
+  }
+  return context;
+};
+
+const useParamsState = () => {
+  const context = useContext(ParamsStateContext);
+  if (context === undefined) {
+    throw new Error("useParamsState must be used within a ParamsProvider");
+  }
+  return context;
+};
+
+export { ParamsProvider, useParamsState, useParamsDispatch };
